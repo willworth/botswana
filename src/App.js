c57d6db4752d38754be4e0266899bc15418cf73d
Will Worth <will.worth@gmail.com>
2018-07-15 17:52:49 +0200

    separate intro component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
+
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import CreateComponent from './components/CreateComponent';
 import EditComponent from './components/EditComponent';
 import IndexComponent from './components/IndexComponent';
+import IntroComponent from './components/IntroComponent';
 
 class App extends Component {
   render() {
     return (
     <Router>
-        <div>
-          <h2>Welcome to React Express Song Saver</h2>
-          <ul>
-          <li><Link to={'/'}>Home</Link></li>
-            <li><Link to={'/create'}>Create</Link></li>
-            <li><Link to={'/index'}>List</Link></li>
-            <li><Link to={'/edit/:id'}>Edit</Link></li>
-          </ul>
-          <hr />
+        <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand">Song Saver!</a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                  <Link to={'/'} className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/create'} className="nav-link">Create</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/index'} className="nav-link">Index</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/edit/:id'} className="nav-link">Edit</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
           <Switch>
               <Route exact path='/create' component={CreateComponent} />
               <Route path='/edit/:id' component={EditComponent} />
               <Route path='/index' component={IndexComponent} />
           </Switch>
+          <IntroComponent />
         </div>
       </Router>
     );
---
+++ src/components/IntroComponent.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class IntroComponent extends Component {
+    render() {
+        return (
+            <div>
+            <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">This is Song Saver.</h1>
+          <p>A simple note taking app aimed at people who sing songs and want to manage their repertoire.</p>
+          <p>The idea is you can group songs together in various ways to create setlists, or use the app to help you 
+            practise before any of your songs get rusty.</p>
+          <p><a class="btn btn-dark btn-lg" href="/create" role="button">Add your first song</a></p>
+        </div>
+      </div>
+            </div>
+        )
+    }
+}
