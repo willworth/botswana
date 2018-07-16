42ea948c90ddb4376151253fc56e8e4984c4f4ae
Will Worth <will.worth@gmail.com>
2018-07-16 21:19:48 +0200

    set state in form

--- src/components/CreateComponent.js
+++ src/components/CreateComponent.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
 
 export default class CreateComponent extends Component {
-    
+
+    constructor(props) {
+        super(props);
+        this.onChangeSongName = this.onChangeSongName.bind(this);
+        this.onChangeArtist = this.onChangeArtist.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            song: '',
+            artist: ''
+        }
+    }
+    onChangeSongName(e) {
+        this.setState({
+            song: e.target.value
+        });
+    }
+    onChangeArtist(e) {
+        this.setState({
+            artist: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        console.log(`Song is ${this.state.song} and artist is ${this.state.artist}`);
+        this.setState({
+            song: '',
+            artist: ''
+        })
+    }
+
+ 
     render() {
         return (
              <div style={{marginTop: 50}}>
                 <h3>Add New Song</h3>
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div className="form-group">
                         <label>Song Name:  </label>
-                        <input type="text" className="form-control"/>
+                        <input type="text" value={this.state.song} className="form-control" onChange={this.onChangeSongName}/>
                     </div>
                     <div className="form-group">
                         <label>Artist: </label>
-                        <input type="text" className="form-control"/>
+                        <input type="text" value={this.state.artist} className="form-control" onChange={this.onChangeArtist}/>
                     </div>
-                    <div className="form-group">
-                        <label>Lyrics/Chords: </label>
-                        <input type="text" className="form-control"/>
-                    </div>
+                   
                     <div className="form-group">
                         <input type="submit" value="Add Song" className="btn btn-primary"/>
                     </div>
@@ -31,62 +60,37 @@ export default class CreateComponent extends Component {
 
 
 
-//////////////old/////////////////
 
-// CreateComponent.js
+
+/////////////////unmoded  will version
 
 // import React, { Component } from 'react';
 
 // export default class CreateComponent extends Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.onChangeHostName = this.onChangeHostName.bind(this);
-//         this.onChangePort = this.onChangePort.bind(this);
-//         this.onSubmit = this.onSubmit.bind(this);
-
-//         this.state = {
-//             name: '',
-//             port: ''
-//         }
-//     }
-//     onChangeHostName(e) {
-//         this.setState({
-//             name: e.target.value
-//         });
-//     }
-//     onChangePort(e) {
-//         this.setState({
-//             port: e.target.value
-//         });
-//     }
-//     onSubmit(e) {
-//         e.preventDefault();
-//         console.log(`name is ${this.state.name} and port is ${this.state.port}`);
-//         this.setState({
-//             name: '',
-//             port: ''
-//         })
-//     }
-
+    
 //     render() {
 //         return (
-//             <div style={{marginTop: 50}}>
-//                 <h3>Add New Server</h3>
-//                 <form onSubmit={this.onSubmit}>
+//              <div style={{marginTop: 50}}>
+//                 <h3>Add New Song</h3>
+//                 <form>
 //                     <div className="form-group">
-//                         <label>Add Host Name:  </label>
-//                         <input type="text" value={this.state.name} className="form-control" onChange={this.onChangeHostName}/>
+//                         <label>Song Name:  </label>
+//                         <input type="text" className="form-control"/>
 //                     </div>
 //                     <div className="form-group">
-//                         <label>Add Server Port: </label>
-//                         <input type="text" value={this.state.port} className="form-control" onChange={this.onChangePort}/>
+//                         <label>Artist: </label>
+//                         <input type="text" className="form-control"/>
 //                     </div>
 //                     <div className="form-group">
-//                         <input type="submit" value="Add Node server" className="btn btn-primary"/>
+//                         <label>Lyrics/Chords: </label>
+//                         <input type="text" className="form-control"/>
+//                     </div>
+//                     <div className="form-group">
+//                         <input type="submit" value="Add Song" className="btn btn-primary"/>
 //                     </div>
 //                 </form>
 //             </div>
 //         )
 //     }
 // }
+
